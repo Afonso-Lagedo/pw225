2c9a71e2e642810067964848e3e2bbf40d317ca0
Afonso-Lagedo <afonso.lagedo@gmail.com>
2025-08-06 09:48:58 -0300

    View

--- db.php
+++ db.php
@@ -6,3 +6,7 @@ $senha = "";
 $db = "teste";
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $db);
+
+
+$query = "SELECT *FROM ALUNOS";
+$consulta_alunos = mysqli_query($conexao, $query);
--- views/teste.php
+++ views/teste.php
@@ -1,3 +1,29 @@
 ROLA
 
 <a href="?pagina=teste2">CU teste2</a>
+
+
+
+<a href="?pagina=inserir_aluno">Inserir novo aluno</a>
+
+<table style="border: 1px solid #000; width:100% " >
+	<thead>
+		<tr>
+			<th>Nome </th>
+			<th>OBS</th>
+			
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+			while($linha = mysqli_fetch_array($consulta_alunos)){
+				echo'<tr><td>'.$linha['nome'].'</td>';
+				echo'<td>'.$linha['obs'].'</td>	';
+			?>
+		<?php
+			}
+		
+		?>
+	</tbody>
+</table>
